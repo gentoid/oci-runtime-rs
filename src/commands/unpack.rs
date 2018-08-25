7b81ba560ed4f14cd16fb08869ef6c541843e5cf
Viktor Lazarev <taurus101v@gmail.com>
2018-08-25 21:07:12 +0200

    Detect GZip file type before unpacking

--- src/commands/unpack.rs
+++ src/commands/unpack.rs
@@ -1,4 +1,5 @@
 use std::io::prelude::*;
+use std::io::SeekFrom;
 use std::fs::File;
 use flate2::read::GzDecoder;
 use tar::Archive;
@@ -7,10 +8,23 @@ use errors::*;
 
 pub fn exec(filename: &str, unpack_to: &str) -> Result<String> {
     let file = File::open(filename).map_err(|err| format!("Cannot open file to read from: {:?}", err))?;
-    let mut data: Vec<u8> = vec![];
+    let mut header = [0; 5];
 
-    GzDecoder::new(file).read_to_end(&mut data).map_err(|err| format!("Cannot read gzipped data: {:?}", err))?;
-    Archive::new(&data[..]).unpack(unpack_to).map_err(|err| format!("There was an error while unpack tar'ed image: {:?}", err))?;
+    let mut handle = file.take(5);
+    handle.read(&mut header).map_err(|err| format!("Cannot open read file {}: {:?}", filename, err))?;
+
+    let mut file = handle.get_ref();
+    file.seek(SeekFrom::Start(0)).map_err(|err| format!("Cannot reset cursor for {}: {:?}", filename, err))?;
+
+    match header {
+        [0x1f, 0x8b, 0x08, _, _] => {
+            println!("We've found a GZip archive");
+            let mut data = Vec::new();
+            GzDecoder::new(file).read_to_end(&mut data).map_err(|err| format!("Cannot read gzipped data: {:?}", err))?;
+            Archive::new(&data[..]).unpack(unpack_to).map_err(|err| format!("There was an error while unpack tar'ed image: {:?}", err))?;
+        }
+        _ => return Err(format!("We cannot recognize file format for {}", filename).into())
+    }
 
     Ok(format!("{:?} is successfully unpacked!", filename))
 }
