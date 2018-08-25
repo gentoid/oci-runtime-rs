ab953951f853c0db035ab378d38cb401c9185c18
Viktor Lazarev <taurus101v@gmail.com>
2018-08-25 18:40:22 +0200

    Use error-chain to simplify error handling in unpack

--- src/commands/unpack.rs
+++ src/commands/unpack.rs
@@ -3,22 +3,14 @@ use std::fs::File;
 use flate2::read::GzDecoder;
 use tar::Archive;
 
-pub fn exec(file: &str, unpack_to: &str) -> Result<String, String> {
-    match File::open(file) {
-        Ok(fd) => {
-            let mut data: Vec<u8> = vec![];
-            let mut gz = GzDecoder::new(fd);
+use errors::*;
 
-            match gz.read_to_end(&mut data) {
-                Ok(_) => {
-                    match Archive::new(&data[..]).unpack(unpack_to) {
-                        Ok(()) => Ok("Successfully unpacked!".to_owned()),
-                        Err(err) => Err(format!("There was an error while unpack: {:?}", err)),
-                    }
-                },
-                Err(what) => Err(format!("Cannot read gzip: {}", what))
-            }
-        }
-        Err(e) => Err(format!("Cannot open file to read from: {}", e))
-    }
+pub fn exec(filename: &str, unpack_to: &str) -> Result<String> {
+    let file = File::open(filename).map_err(|err| format!("Cannot open file to read from: {:?}", err))?;
+    let mut data: Vec<u8> = vec![];
+
+    GzDecoder::new(file).read_to_end(&mut data).map_err(|err| format!("Cannot read gzipped data: {:?}", err))?;
+    Archive::new(&data[..]).unpack(unpack_to).map_err(|err| format!("There was an error while unpack tar'ed image: {:?}", err))?;
+
+    Ok(format!("{:?} is successfully unpacked!", filename))
 }
