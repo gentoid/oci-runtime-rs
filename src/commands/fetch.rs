70a2b8868f31ca5c5025d2b36c4891f794bb0d2a
Viktor Lazarev <taurus101v@gmail.com>
2018-08-25 12:45:01 +0200

    Use clap to handle CLI options

--- src/commands/fetch.rs
+++ src/commands/fetch.rs
@@ -16,11 +16,12 @@ impl<'cn> ContainerName<'cn> {
     }
 }
 
-pub fn exec(url: &str) -> Result<String, String> { // TODO: do not use stringified API's
+pub fn exec(url: &str, save_to: &str) -> Result<String, String> { // TODO: do not use stringified API's
+    println!("{:?}", save_to);
     // ContainerName::parse(container_name).map(fetch).ok_or("Cannot parse container name")
     match reqwest::get(url) {
         Ok(mut resp) => {
-            match File::create("image.aci") {
+            match File::create(save_to) {
                 Ok(mut file) => {
                     match resp.copy_to(& mut file) {
                         Ok(bytes) => Ok(format!("Image just has been fetched and wrote {} bytes", bytes)),
@@ -29,7 +30,6 @@ pub fn exec(url: &str) -> Result<String, String> { // TODO: do not use stringifi
                 }
                 Err(r) => Err(format!("Cannot open file {}", r))
             }
-
         }
         Err(w) => Err(format!("Cannot get data {}", w))
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
+extern crate clap;
 extern crate reqwest;
 
 mod commands;
 
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    match commands::fetch::exec("https://github.com/coreos/etcd/releases/download/v3.3.9/etcd-v3.3.9-linux-amd64.aci") {
-        Ok(res) => println!("{:?}", res),
-        Err(err) => print!("{:?}", err),
+    let matches = App::new("roci")
+        .version("0.1.0")
+        .author("Viktor L.")
+        .about("An application to run containers")
+        .subcommand(SubCommand::with_name("fetch")
+            .about("Fetches images from remote registries")
+            .arg(Arg::with_name("IMAGE_URI")
+                .help("Image URI to fetch")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("SAVE_TO")
+                .help("Where it should save fetched image. Default is: image.aci")
+                .required(false)
+                .index(2)))
+        .get_matches();
+
+    if let Some(fetch_match) = matches.subcommand_matches("fetch") {
+        let uri = fetch_match.value_of("IMAGE_URI").unwrap();
+        let save_to = fetch_match.value_of("SAVE_TO").unwrap_or("image.aci");
+        match commands::fetch::exec(uri, save_to) {
+            Ok(res) => println!("{:?}", res),
+            Err(err) => print!("{:?}", err),
+        }
     }
 }
