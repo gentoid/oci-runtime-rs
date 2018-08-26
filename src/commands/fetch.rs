4c3bfad2c938f730dc47df5a8083dbf4daaf3580
Viktor Lazarev <taurus101v@gmail.com>
2018-08-26 10:52:31 +0200

    Check reponse status before storing data

--- src/commands/fetch.rs
+++ src/commands/fetch.rs
@@ -21,6 +21,13 @@ impl<'cn> ContainerName<'cn> {
 pub fn exec(url: &str, save_to: &str) -> Result<String> { // TODO: do not use stringified API's
     let mut file = File::create(save_to).map_err(|err| format!("Cannot open file to save image to: {}", err))?;
     let mut resp = reqwest::get(url).map_err(|err| format!("Cannot get data: {:?}", err))?;
+    let status = resp.status();
+    if !status.is_success() {
+        return Err(format!(
+            "Status code isn't success: {} {:?}",
+            status.as_u16(),
+            status.canonical_reason().unwrap_or("Unknown reason")).into())
+        }
     resp.copy_to(& mut file).map_err(|err| format!("Cannot write data: {:?}", err))?;
     Ok(format!("Image has been saved to {:?}", save_to))
 }
