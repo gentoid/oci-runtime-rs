47460ed48bf7b94b957a74cb96f4d0c12edc15ac
Viktor Lazarev <taurus101v@gmail.com>
2018-08-26 12:20:23 +0200

    Try to collect gpg keys

--- src/commands/fetch.rs
+++ src/commands/fetch.rs
@@ -1,6 +1,6 @@
 use reqwest;
 use std::fs::File;
-use scraper::{Html, Selector};
+use scraper::{Html, Selector, ElementRef};
 
 use errors::*;
 
@@ -19,8 +19,8 @@ impl<'cn> ContainerName<'cn> {
     }
 }
 
-pub fn exec(url: &str, save_to: &str) -> Result<String> { // TODO: do not use stringified API's
-    let url = format!("https://{}?ac-discovery=1", url);
+pub fn exec(name: &str, save_to: &str) -> Result<String> { // TODO: do not use stringified API's
+    let url = format!("https://{}?ac-discovery=1", name);
     let mut file = File::create(save_to).map_err(|err| format!("Cannot open file to save image to: {}", err))?;
     let mut resp = reqwest::get(&url).map_err(|err| format!("Cannot get data: {:?}", err))?;
     let status = resp.status();
@@ -33,11 +33,20 @@ pub fn exec(url: &str, save_to: &str) -> Result<String> { // TODO: do not use st
 
     let body = resp.text().map_err(|err| format!("Cannot read body of the response: {}", err))?;
     let document = Html::parse_document(&body);
-    let meta_tag = Selector::parse("meta").map_err(|err| format!("Provided wrong HTML tag: {:?}", err))?;
+
+    let tag_selector = format!("meta[name=ac-discovery-pubkeys][content^=\"{} \"]", name);
+    let meta_tag = Selector::parse(&tag_selector)
+        .map_err(|err| format!("Provided wrong CSS selector: {:?}", err))?;
 
     for tag in document.select(&meta_tag) {
         println!("{:?}", tag.value().attrs().collect::<Vec<_>>());
     }
+
+    let keys: Vec<&str> = document.select(&meta_tag)
+        .map(|tag| tag.value().attr("content").unwrap())
+        .collect();
+
+    println!("{:?}", keys);
 
     resp.copy_to(& mut file).map_err(|err| format!("Cannot write data: {:?}", err))?;
     Ok(format!("Image has been saved to {:?}", save_to))
